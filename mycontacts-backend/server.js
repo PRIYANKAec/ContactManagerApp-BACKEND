9fe45e6c460da44f2ba2d1fee50f2c686464ab36
PRIYANKAec <priyankaa261103@gmail.com>
2024-08-16 11:02:42 +0530

    Implemented sending req to backen json format

--- mycontacts-backend/server.js
+++ mycontacts-backend/server.js
@@ -4,6 +4,14 @@ const app = express();
 
 const port = process.env.PORT || 5000;
 
+app.get("/api/contacts",(req,res) =>
+{
+    //sending message to backend as an normal message
+    // res.send("Getting All contacts"); 
+    res.status(200).json({message:"Get all contacts"}); //json format with status code
+    
+})
+
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 })
