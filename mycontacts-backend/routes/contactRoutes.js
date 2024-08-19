b9ef7a9019515debd378e146c538d67bd304ed08
PRIYANKAec <priyankaa261103@gmail.com>
2024-08-19 20:00:14 +0530

    Implemented the routing using the controllers by exporting

--- mycontacts-backend/routes/contactRoutes.js
+++ mycontacts-backend/routes/contactRoutes.js
@@ -1,34 +1,14 @@
 const express = require("express");
 const router = express.Router(); //router creation
+const {
+getContacts, 
+createContact, 
+getContact, 
+updateContact, 
+deleteContact
+} = require("../controllers/contactController");
 
-//get method
-router.route("/").get((req,res) =>
-{
-    res.status(200).json({message:"Get all contacts"});
-});
-
-//post method
-router.route("/").post((req,res) =>
-{
-    res.status(200).json({message:"Create Contact"});
-});
-
-//get router 
-router.route("/:id").get((req,res) =>
-{
-    res.status(200).json({message:`Get contact for ${req.params.id}` });
-});
-
-//put router
-router.route("/:id").put((req,res) =>
-{
-    res.status(200).json({message:`Update contact for ${req.params.id}` });
-});
-
-//delete method
-router.route("/:id").delete((req,res) =>
-{
-    res.status(200).json({message:`Delete contact for ${req.params.id}`});
-});
+router.route("/").get(getContacts).post(createContact); //passing without id
+router.route("/:id").get(getContact).put(updateContact).delete(deleteContact); // passing with id 
 
 module.exports = router;
