56e53bb971259dbcc49b6b44479edf591940a96a
PRIYANKAec <priyankaa261103@gmail.com>
2024-08-24 11:10:53 +0530

    Error Handling in server and client side and displaying the error in JSON format[ADD]

--- mycontacts-backend/controllers/contactController.js
+++ mycontacts-backend/controllers/contactController.js
@@ -12,6 +12,14 @@ const getContacts = (req,res) =>
 const createContact = (req,res) =>
 {
     console.log("The request Body is :",req.body); // retriving the data from the server
+
+    //error handling through express
+    const { name, email, phone } = req.body;
+    if(!name || !email || !phone)
+    {
+        res.status(400);
+        throw new Error("All fields are mandatory: ");
+    }
     res.status(200).json({message:"Create Contact"});
 };
 
--- mycontacts-backend/server.js
+++ mycontacts-backend/server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const errorHandler = require("./middleware/errorHandler");
 const dotenv = require("dotenv").config(); 
 const app = express();
 
@@ -7,6 +8,8 @@ const port = process.env.PORT || 5000;
 app.use(express.json()); //middleware to pass the data from client to server
 
 app.use("/api/contacts",require("./routes/contactRoutes")); // URL for the hosting 
+
+app.use(errorHandler);
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
