eb3b71d24eed1fa8ddb376efef29ab3c09b0d800
PRIYANKAec <priyankaa261103@gmail.com>
2024-09-01 12:49:52 +0530

    Added UserController

---
+++ mycontacts-backend/controllers/userController.js
@@ -0,0 +1,28 @@
+const asyncHandler = require("express-async-handler");
+
+//@desc Register a user
+//@route POST /api/users/register
+//@access public
+const registerUser = asyncHandler(async (req,res) => {
+    res.json({message: "Register the user"});
+   }
+);
+
+//@desc login a user
+//@route POST /api/users/login
+//@access public
+const loginUser = asyncHandler(async (req,res) => {
+    res.json({message: "Login  user"});
+   }
+);
+
+//@desc user info
+//@route GET /api/users/current
+//@access private
+const currentUser = asyncHandler(async  (req,res) => {
+    res.json({message: "Current user information"});
+   }
+);
+
+
+module.exports = { registerUser  , loginUser, currentUser };
