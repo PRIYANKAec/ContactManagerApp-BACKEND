25ddb367a42dc9d540a75d3c265fdd00a2d570e2
PRIYANKAec <priyankaa261103@gmail.com>
2024-09-01 11:41:07 +0530

    Delete Contact in database[ADD]

--- mycontacts-backend/controllers/contactController.js
+++ mycontacts-backend/controllers/contactController.js
@@ -72,7 +72,15 @@ const updateContact = asyncHandler(async(req,res) =>
 //@access public
 const deleteContact = asyncHandler(async(req,res) =>
 {
-    res.status(201).json({message:`Delete contact for ${req.params.id}`});
+    const contact = await Contact.findById(req.params.id);
+    if(!contact)
+    {
+        res.status(404);
+        throw new Error("Contact not found");
+    }
+    
+    await Contact.remove();
+    res.status(200).json(contact);
 });
 
 module.exports = {
