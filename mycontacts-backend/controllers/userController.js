01285d26120716a0e53e3821bd7254da2ab12d40
PRIYANKAec <priyankaa261103@gmail.com>
2024-09-01 20:58:14 +0530

    Implemented jwt token accesstoken[ADD]

--- mycontacts-backend/controllers/userController.js
+++ mycontacts-backend/controllers/userController.js
@@ -1,5 +1,6 @@
 const asyncHandler = require("express-async-handler");
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 const User = require("../models/userModel")
 //@desc Register a user
 //@route POST /api/users/register
@@ -42,9 +43,33 @@ const registerUser = asyncHandler(async (req,res) => {
 //@route POST /api/users/login
 //@access public
 const loginUser = asyncHandler(async (req,res) => {
-    res.json({message: "Login  user"});
-   }
-);
+    const {email, password} = req.body;
+    if(!email || !password) {
+        res.status(400);
+        throw new Error("All fields are mandatory");
+    }
+    const user = await User.findOne({ email });
+    // compare original password with the hashed password by encrypting 
+    if(user && (await bcrypt.compare(password, user.password))) {
+        const accessToken = jwt.sign(
+            {
+            //payload
+            user: {
+                username: user.username,
+                email: user.email,
+                id: user.id,
+            },
+        },
+        process.env.ACCESS_TOKEN_SECRET,
+        { expiresIn: "1n"}
+    );
+        res.status(200).json({ accessToken });
+    }
+    else {
+        res.status(401);
+        throw new Error("email and password is not valid");
+    }
+   });
 
 //@desc user info
 //@route GET /api/users/current
