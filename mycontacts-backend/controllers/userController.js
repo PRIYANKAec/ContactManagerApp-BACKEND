9e1cd231315014ee7394a4adf5c4a33ef1e11d30
PRIYANKAec <priyankaa261103@gmail.com>
2024-09-01 17:30:56 +0530

    Created new user,implemented hashed password using bcrypt,checking user already exists[Authenticated][ADD]

--- mycontacts-backend/controllers/userController.js
+++ mycontacts-backend/controllers/userController.js
@@ -1,9 +1,39 @@
 const asyncHandler = require("express-async-handler");
-
+const bcrypt = require("bcrypt");
+const User = require("../models/userModel")
 //@desc Register a user
 //@route POST /api/users/register
 //@access public
 const registerUser = asyncHandler(async (req,res) => {
+    const {username, email, password } = req.body;
+    if(!username || !email || !password) {
+        res.status(400);
+        throw new Error("All fields are mandatory");
+    }
+     // if the user alredy exists
+    const userAvailable = await User.findOne({email});
+    if(userAvailable) {
+        res.status(400);
+        throw new Error("User already registered");
+    }
+
+    //Hashing the  password
+    const hashedPassword = await bcrypt.hash(password,10);
+    console.log("Hashed Password:", hashedPassword);
+    // creating the new user
+    const user = await User.create({
+        username,
+        email,
+        password: hashedPassword,
+    });
+    console.log(`User created ${user}`);
+    // user visualizing data 
+    if(user) {
+        res.status(201).json({ _id: user.id, email:user.email});
+        } else {
+        res.status(400);
+        throw new Error("User data is not valid");
+    }
     res.json({message: "Register the user"});
    }
 );
